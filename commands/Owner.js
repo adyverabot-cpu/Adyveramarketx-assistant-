30f09f51f9fd66f615c3e552699fa3c5cce5547c
adyverabot-cpu <adyveramarketx@gmail.com>
2026-05-02 11:16:59 +0700

    Update Owner.js

--- commands/Owner.js
+++ commands/Owner.js
@@ -1,6 +1,29 @@
 module.exports = async (sock, jid, config) => {
-await sock.sendMessage(jid,{
-text:`Owner: ${config.ownerName}
-wa.me/${config.ownerNumber}`
-})
+try {
+
+let teks = `
+╔══ OWNER ADYVERA ══╗
+
+👑 Nama : ${config.ownerName}
+📱 Nomor : ${config.ownerNumber}
+
+📞 Chat Owner:
+https://wa.me/${config.ownerNumber}
+
+💼 Layanan:
+• Top Up Game
+• Jual Beli Akun
+• Rekber
+• MC
+• Jasa Grup
+
+🔥 Powered by ${config.botName}
+╚═══════════════╝
+`
+
+await sock.sendMessage(jid,{text:teks})
+
+} catch(err){
+await sock.sendMessage(jid,{text:"Owner error"})
+}
 }
