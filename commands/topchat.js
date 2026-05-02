9a92fdc8496dcce3be9380c8d47e49174d3520c0
adyverabot-cpu <adyveramarketx@gmail.com>
2026-05-02 13:14:26 +0700

    Update topchat.js

--- commands/topchat.js
+++ commands/topchat.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
-module.exports = async (sock,jid) => {
+module.exports = async (sock, jid) => {
 
 let db = JSON.parse(fs.readFileSync('./database/users.json'))
 
 let data = Object.entries(db)
-.sort((a,b)=>b[1].chat-a[1].chat)
+.sort((a,b) => b[1].chat - a[1].chat)
 .slice(0,10)
 
 let teks = '💬 TOP CHAT USER 💬\n\n'
@@ -14,5 +14,6 @@ data.forEach((v,i)=>{
 teks += `${i+1}. ${v[0].split('@')[0]} - ${v[1].chat} chat\n`
 })
 
-sock.sendMessage(jid,{text:teks})
-  }
+await sock.sendMessage(jid,{ text: teks })
+
+}
