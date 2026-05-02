78b989696d4c5ef454c8294c358a132092e4bc49
adyverabot-cpu <adyveramarketx@gmail.com>
2026-05-02 11:20:53 +0700

    Update Topup.js

--- commands/Topup.js
+++ commands/Topup.js
@@ -1,23 +1,125 @@
-module.exports = async (sock, jid, game, owner) => {
+module.exports = async (sock, jid, type, owner) => {
 try {
 
-let teks = `
-╔══ TOP UP ${game.toUpperCase()} ══╗
+let game = type.toUpperCase()
+let teks = ""
 
-Silakan order ke owner:
+if(type === "ml"){
+teks = `
+╔══ TOP UP MOBILE LEGENDS ══╗
 
-📱 wa.me/${owner}
+💎 Diamond murah & cepat
+📱 Order ke owner:
+wa.me/${owner}
 
-💸 Harga murah
-⚡ Proses cepat
-✅ Aman & terpercaya
+Format:
+.topupml
 
-Powered by ADYVERA TV
+🔥 ADYVERA TV
 `
+}
+
+else if(type === "ff"){
+teks = `
+╔══ TOP UP FREE FIRE ══╗
+
+💎 Diamond / Membership
+📱 Order ke owner:
+wa.me/${owner}
+
+Format:
+.topupff
+
+🔥 ADYVERA TV
+`
+}
+
+else if(type === "pubg"){
+teks = `
+╔══ TOP UP PUBG MOBILE ══╗
+
+💎 UC cepat masuk
+📱 Order ke owner:
+wa.me/${owner}
+
+Format:
+.topuppubg
+`
+}
+
+else if(type === "valorant"){
+teks = `
+╔══ TOP UP VALORANT ══╗
+
+💎 VP murah aman
+📱 Order ke owner:
+wa.me/${owner}
+
+Format:
+.topupvalorant
+`
+}
+
+else if(type === "pulsa"){
+teks = `
+╔══ ISI PULSA ══╗
+
+📱 Semua Operator
+📞 Order:
+wa.me/${owner}
+
+Format:
+.pulsa
+`
+}
+
+else if(type === "data"){
+teks = `
+╔══ PAKET DATA ══╗
+
+📶 Semua Provider
+📱 Order:
+wa.me/${owner}
+
+Format:
+.data
+`
+}
+
+else if(type === "voucher"){
+teks = `
+╔══ VOUCHER DIGITAL ══╗
+
+🎫 Google Play
+🎫 Garena
+🎫 Steam
+🎫 dll
+
+📱 Order:
+wa.me/${owner}
+
+Format:
+.voucher
+`
+}
+
+else {
+teks = `
+╔══ TOPUP MENU ══╗
+.topupml
+.topupff
+.topuppubg
+.topupvalorant
+.pulsa
+.data
+.voucher
+╚══════════════╝
+`
+}
 
 await sock.sendMessage(jid,{text:teks})
 
 } catch(err){
-await sock.sendMessage(jid,{text:"Topup error"})
+await sock.sendMessage(jid,{text:"Topup system error"})
 }
 }
