ec9801ed10f9e1f99f28e2c58ae3da1a4c309e7b
adyverabot-cpu <adyveramarketx@gmail.com>
2026-05-02 12:37:09 +0700

    Create Toptrx.js

---
+++ commands/Toptrx.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+
+module.exports = async (sock,jid) => {
+
+let db = JSON.parse(fs.readFileSync('./database/users.json'))
+
+let data = Object.entries(db)
+.sort((a,b)=>b[1].trx-a[1].trx)
+.slice(0,10)
+
+let teks = '🛒 TOP TRANSAKSI 🛒\n\n'
+
+data.forEach((v,i)=>{
+teks += `${i+1}. ${v[0].split('@')[0]} - ${v[1].trx} trx\n`
+})
+
+sock.sendMessage(jid,{text:teks})
+}
