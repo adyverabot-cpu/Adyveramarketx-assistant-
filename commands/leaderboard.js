e5561e5ac138edcf6601e2a1de0a9b7258733117
adyverabot-cpu <adyveramarketx@gmail.com>
2026-05-02 13:11:15 +0700

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
 
-module.exports = async (sock,jid) => {
+module.exports = async (sock, jid) => {
 
 let db = JSON.parse(fs.readFileSync('./database/users.json'))
 
 let data = Object.entries(db).sort((a,b)=>{
-let pa = a[1].chat + a[1].koin + (a[1].trx*10)
-let pb = b[1].chat + b[1].koin + (b[1].trx*10)
+let pa = a[1].chat + a[1].koin + (a[1].trx * 10)
+let pb = b[1].chat + b[1].koin + (b[1].trx * 10)
 return pb - pa
 }).slice(0,10)
 
@@ -17,5 +17,6 @@ teks += `${i+1}. ${v[0].split('@')[0]}\n`
 teks += `💬${v[1].chat} 🪙${v[1].koin} 🛒${v[1].trx}\n\n`
 })
 
-sock.sendMessage(jid,{text:teks})
+await sock.sendMessage(jid,{ text: teks })
+
 }
